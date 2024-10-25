09516203565675943cf57e98aad1c697fdf79eaa
xiaohuan <1305584373@qq.com>
2024-10-25 22:00:41 +0800

    增加弹窗

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,44 @@
 <script setup>
 import { Delete, Edit } from "@element-plus/icons-vue";
 import { ElMessage } from "element-plus";
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
+
+const dialogVisible = ref(false);
+
+const sessionItem = reactive({
+  id: "",
+  title: "",
+});
 
 const session = reactive(JSON.parse(localStorage.getItem("session")) || []);
-const onClick = () => {
-  const title = prompt("请输入会话标题");
-  session.push({
-    id: Date.now(),
-    title,
-  });
+
+const onUpdate = () => {
+  if (sessionItem.title) {
+    if (sessionItem.id) {
+      const index = session.findIndex((item) => item.id === sessionItem.id);
+      session.splice(index, 1, {
+        id: sessionItem.id,
+        title: sessionItem.title,
+      });
+    } else {
+      session.push({
+        id: Date.now(),
+        title: sessionItem.title,
+      });
+    }
+  } else {
+    ElMessage.error("会话标题不能为空");
+    return;
+  }
+
   localStorage.setItem("session", JSON.stringify(session));
+  onQuit();
+};
+
+const onQuit = () => {
+  sessionItem.id = "";
+  sessionItem.title = "";
+  dialogVisible.value = false;
 };
 
 const onDelete = (id) => {
@@ -24,21 +52,35 @@ const onDelete = (id) => {
 };
 
 const onEdit = (id) => {
-  const index = session.findIndex((item) => item.id === id);
-  const title = prompt("请修改会话标题", session[index].title);
-  session.splice(index, 1, { id, title });
-  localStorage.setItem("session", JSON.stringify(session));
+  dialogVisible.value = true;
+  sessionItem.id = id;
+  const index = session.findIndex((item) => item.id === sessionItem.id);
+  sessionItem.title = session[index].title;
 };
 </script>
 
 <template>
   <div>
-    <el-button type="primary" @click="onClick" class="btn">添加会话</el-button>
+    <el-button type="primary" @click="dialogVisible = true" class="btn"
+      >添加会话</el-button
+    >
     <div class="title" v-for="item in session" :key="item.id">
       <span style="margin-right: 8px">{{ item.title }}</span>
       <el-link class="icon" :icon="Delete" @click="onDelete(item.id)"></el-link>
       <el-link class="icon" :icon="Edit" @click="onEdit(item.id)"></el-link>
     </div>
+
+    <!-- 弹窗 -->
+    <el-dialog v-model="dialogVisible" width="400">
+      {{ sessionItem.id ? "请修改会话标题：" : "请输入会话标题：" }}
+      <el-input v-model="sessionItem.title"></el-input>
+      <template #footer>
+        <div>
+          <el-button @click="onQuit">取消</el-button>
+          <el-button type="primary" @click="onUpdate">确认</el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
